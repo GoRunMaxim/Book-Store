dcff38e1738ed3ea92f2128e3dbdd68278d1cd60
Max <maxim.peshko@mail.ru>
2021-10-19 00:35:21 +0300

    Add book table diagram & change main cmd file

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,33 @@
-package cmd
+package main
+
+import (
+	"Itechart/BookStore/Book-Store/internal/app"
+	"Itechart/BookStore/Book-Store/internal/config"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+// AppName is the name of application. We use it for logging messages
+const AppName = "BookStore: "
+
+func main() {
+	logrus.Info(AppName + "[" + time.Now().Format(time.RFC822) + "] " + "Start loading config...")
+	c := config.AppConfig{}
+	if err := c.Load(config.SERVICENAME); err != nil {
+		logrus.Fatalf(AppName+"["+time.Now().Format(time.RFC822)+"] "+"Can't load config, missing environment variable. Error:", err.Error())
+	}
+	logrus.Info(AppName + "[" + time.Now().Format(time.RFC822) + "] " + "Successfully load config")
+	logrus.Info(AppName + "[" + time.Now().Format(time.RFC822) + "] " + "Start initializing application...")
+	router, err := app.Initialize(c)
+	if err != nil {
+		logrus.Fatalf(AppName+"["+time.Now().Format(time.RFC822)+"] "+"Can't initialize application. Error:", err.Error())
+	}
+	logrus.Info(AppName + "[" + time.Now().Format(time.RFC822) + "] " + "Application has been initialized")
+	logrus.Info(AppName + "[" + time.Now().Format(time.RFC822) + "] " + "Usual router is listeting ...")
+	err = http.ListenAndServe("0.0.0.0:8080", router)
+	if err != nil {
+		logrus.Fatalf(AppName+"["+time.Now().Format(time.RFC822)+"] "+"Service shutdown. Error:", err.Error())
+	}
+}
---
+++ internal/app/app.go
@@ -0,0 +1,73 @@
+package app
+
+import (
+	"Itechart/BookStore/Book-Store/internal/config"
+	"Itechart/BookStore/Book-Store/internal/controllers"
+	"Itechart/BookStore/Book-Store/internal/handlers"
+	"Itechart/BookStore/Book-Store/internal/postgreSql"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	// ApplicationName is the name of app. Use it for logrus messages
+	ApplicationName = "BookStore: "
+)
+
+// Initialize performs required application configuration.
+func Initialize(cfg config.AppConfig) (externalRouter *mux.Router, err error) {
+	err = initializeLogger(cfg.LogConfig)
+	if err != nil {
+		logrus.Errorf(ApplicationName+"["+time.Now().Format(time.RFC822)+"] "+"Cannot initialize Logger: ", err.Error())
+		return nil, err
+	}
+
+	controller, err := initializeAPIController(cfg)
+	if err != nil {
+		logrus.Errorf(ApplicationName+"["+time.Now().Format(time.RFC822)+"] "+"Cannot initialize APIController: ", err.Error())
+		return nil, err
+	}
+
+	router := initializeRouter(controller)
+
+	return router, nil
+}
+
+// initializeAPIController performs required controller configuration.
+func initializeAPIController(cfg config.AppConfig) (handlers.Controller, error) {
+	DB, err := postgreSql.New(cfg.DbConfig)
+	if err != nil {
+		logrus.Errorf(ApplicationName+"["+time.Now().Format(time.RFC822)+"] "+"Cannot create DB: ", err.Error())
+		return nil, err
+	}
+	controller := controllers.New(DB)
+	return controller, nil
+}
+
+func initializeRouter(controller handlers.Controller) *mux.Router {
+	var router = mux.NewRouter()
+	// var handler = handlers.NewHTTPHandler(controller)
+	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {}).Methods("POST")
+
+	return router
+}
+
+func initializeLogger(cfg config.LogConfig) error {
+	if cfg.WriteToFile {
+		f, err := os.OpenFile(cfg.Filepath, os.O_WRONLY|os.O_CREATE, 0755)
+		if err != nil {
+			return err
+		}
+		logrus.SetOutput(f)
+	}
+	level, err := logrus.ParseLevel(cfg.Level)
+	if err != nil {
+		level = logrus.ErrorLevel
+	}
+	logrus.SetLevel(level)
+	return nil
+}
